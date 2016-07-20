d3d14f199db849d76421370430b8ec20cc505953
dashersw <armagan@amcalar.com>
2016-07-20 23:16:41 +0200

    Return a JSON with source & time fields in time service

--- time-service/index.js
+++ time-service/index.js
@@ -1,9 +1,16 @@
 var cote = require('cote');
 
+var hostname = require('os').hostname();
+var source = `time-service@${hostname}`;
+
 var timeResponder = new cote.Responder({
     name: 'Time Responder'
 });
 
 timeResponder.on('time', function(req, cb) {
-    cb(new Date());
+    var time = new Date();
+    var reply = {source, time};
+
+    console.log(reply);
+    cb(reply);
 });
